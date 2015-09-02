d7dee7a68e966f5f0123c4a81254b3fba6ef12e1
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-02 13:30:59 -0700

    swig cache setup

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ var PORT = process.env.PORT || 5000;
 var app = express();
 
 app.engine('html', swig.renderFile);
-
 app.set('view engine', 'html');
 app.set('views',       __dirname + '/views');
+app.set('view cache',  app.get('env') === 'production');
+swig.setDefaults({ cache: app.get('env') === 'production' ? 'memory' : false });
 
 app.get('/', function (req, res) {
     res.render('index');
 });
 
 app.listen(PORT, function() {
-    console.log('Application Started on port', PORT);
+    console.log('Application Started');
 });
