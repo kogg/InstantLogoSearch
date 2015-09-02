004eced71f5f4968a2c023effdd18b087c698fbc
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-02 16:51:23 -0700

    normalizing some stuff

--- models/brand.js
+++ models/brand.js
@@ -1,14 +1,22 @@
 var _ = require('underscore');
 
+var brands = [{ name: 'About.me' },
+              { name: 'Addvocate' },
+              { name: 'Adobe' },
+              { name: 'Aetna' },
+              { name: 'Aim' },
+              { name: 'Baidu' },
+              { name: 'Bandcamp' },
+              { name: 'Barnes & Noble' },
+              { name: 'Bebo' },
+              { name: 'Behance' }];
+brands = _.chain(brands)
+          .each(function(brand) {
+              brand.effective_name = brand.name.toLowerCase().replace(/[^a-z]+/g, '');
+          })
+          .sortBy('effective_name')
+          .value();
+
 exports.all = function(callback) {
-    callback(null, _.sortBy([{ name: 'About.me' },
-                             { name: 'Addvocate' },
-                             { name: 'Adobe' },
-                             { name: 'Aetna' },
-                             { name: 'Aim' },
-                             { name: 'Baidu' },
-                             { name: 'Bandcamp' },
-                             { name: 'Barnes & Noble' },
-                             { name: 'Bebo' },
-                             { name: 'Behance' }], 'name'));
+    callback(null, brands);
 };
