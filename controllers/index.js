fe41caa8f08224ba11d902b5c2fd287a2ed45d03
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-02 16:26:44 -0700

    Pass brands to index

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var Brand   = require('../models/brand');
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.render('index');
+router.get('/', function (req, res, next) {
+    Brand.all(function(err, brands) {
+        if (err) {
+            return next(err);
+        }
+        res.render('index', { brands: brands });
+    });
 });
 
 module.exports = router;
---
+++ models/brand.js
@@ -0,0 +1,12 @@
+exports.all = function(callback) {
+    callback(null, [{ name: 'About.me' },
+                    { name: 'Addvocate' },
+                    { name: 'Adobe' },
+                    { name: 'Aetna' },
+                    { name: 'Aim' },
+                    { name: 'Baidu' },
+                    { name: 'Bandcamp' },
+                    { name: 'Barnes & Noble' },
+                    { name: 'Bebo' },
+                    { name: 'Behance' }]);
+};
