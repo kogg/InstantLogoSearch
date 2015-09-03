80fea43abe9978e070277de1a8e7a76a16c81e3d
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-02 19:31:30 -0700

    triggering the popup

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 $(function() {
+    var $body = $('body');
+
     var $window = $(window);
     var $header, $header_height, $sidebar;
     $window.on('scroll resize', function(e) {
@@ -12,16 +14,24 @@ $(function() {
 
     $('#search-form').on('submit', function(e) {
         e.preventDefault();
+        $('.col-3:visible:first .tile').trigger('brand-popup');
     });
 
     var $search_bar = $('#search-bar');
-    var $body, $filter_style;
+    var $filter_style;
     $search_bar.on('input', function(e) {
         var val = $search_bar.val().toLowerCase().replace(/[^a-z]+/g, '');
         var filtering = !!(val && val.length);
-        $body = $body || $('body');
         $body.toggleClass('filtering', filtering);
         $filter_style = $filter_style || $('<style></style>').appendTo('head');
         $filter_style.text(filtering ? ('.trie-' + val + '{display:block !important;}' + '.group-' + val[0] + '{display:block !important;}') : '');
+    });
+
+    $body.on('click', '.tile', function() {
+        $(this).trigger('brand-popup');
+    });
+
+    $body.on('brand-popup', '.tile', function(e) {
+        console.log('hey', e);
     });
 });
