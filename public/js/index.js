736fc3c6937458c186984074442792a9cb15af79
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-09-02 19:48:49 -0700

    popups can open and close now

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,7 @@ $(function() {
 
     $('#search-form').on('submit', function(e) {
         e.preventDefault();
-        $('.col-3:visible:first .tile').trigger('brand-popup');
+        $('.col-3:visible:first .tile').trigger('open-popup');
     });
 
     var $search_bar = $('#search-bar');
@@ -28,10 +28,22 @@ $(function() {
     });
 
     $body.on('click', '.tile', function() {
-        $(this).trigger('brand-popup');
+        $(this).trigger('open-popup');
     });
 
-    $body.on('brand-popup', '.tile', function(e) {
-        console.log('hey', e);
+    $body.on('open-popup', function() {
+        $body.addClass('popup');
+    });
+
+    $body.on('close-popup', function() {
+        $body.removeClass('popup');
+    });
+
+    $('#logo-popup').on('click', function(e) {
+        e.stopPropagation();
+    });
+
+    $('#logo-popup-container').on('click', function() {
+        $body.trigger('close-popup');
     });
 });
