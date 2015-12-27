98500aa30e99520806a99337cfcf97f9091fc8cd
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-12-26 17:00:16 -0800

    feathers rest/sockio & body-parser

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-module.exports = require('feathers')();
+var feathers   = require('feathers');
+var bodyParser = require('body-parser');
+
+var app = feathers();
+
+app.configure(feathers.rest());
+app.configure(feathers.socketio());
+app.use(bodyParser.json());
+
+module.exports = app;
