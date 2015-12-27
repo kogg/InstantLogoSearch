a4164ba329b077e566b2646b1204634b673c117b
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-12-26 18:15:36 -0800

    urlencoded bodyParser in case

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ var app = feathers();
 app.configure(feathers.rest());
 app.configure(feathers.socketio());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = app;
