ae94d566b1f49a8bbbf24ba0e880813351605fe9
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-12-26 20:56:35 -0800

    more postcss plugins and better stylelint globbing

--- postcss.js
+++ postcss.js
@@ -1,3 +1,17 @@
 module.exports = {
-	use: ['autoprefixer']
+	'use': [
+		'postcss-import',
+		'postcss-nested',
+		'postcss-custom-media',
+		'autoprefixer',
+		'postcss-color-rgba-fallback',
+		'postcss-url'
+	],
+	'postcss-import': {
+		glob: true
+	},
+	'postcss-url': {
+		url:        'copy',
+		assetsPath: 'assets'
+	}
 };
