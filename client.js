8a17c985e7cab88de527bc8711749e28c5cbd87a
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2015-12-28 15:18:31 -0800

    variables are good for requires

--- client.js
+++ client.js
@@ -1,8 +1,7 @@
 var ReactDOM = require('react-dom');
 
+var app = require('./app');
+
 var state = JSON.parse(document.getElementById('react-state').innerHTML);
 
-ReactDOM.render(
-	require('./app')(state),
-	document.getElementById('react-app')
-);
+ReactDOM.render(app(state), document.getElementById('react-app'));
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path     = require('path');
 var ReactDOM = require('react-dom/server');
 
 var express_app = require('./express-app');
+var app         = require('./app');
 
 express_app.set('port', process.env.PORT || 5000);
 
@@ -11,7 +12,7 @@ express_app.set('views', path.join(__dirname, '/views'));
 express_app.get('/', function(req, res) {
 	var state = { message: 'some message' };
 	res.render('main', {
-		markup: ReactDOM.renderToString(require('./app')(state)),
+		markup: ReactDOM.renderToString(app(state)),
 		state:  state
 	});
 });
