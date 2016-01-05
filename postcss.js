904f0608501c8301a5ad915f2105f124ba687380
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-05 01:46:31 -0800

    run stylelint everytime postcss runs

--- postcss.js
+++ postcss.js
@@ -3,6 +3,7 @@ var _ = require('underscore');
 module.exports = {
 	'use': _.union(
 		[
+			'stylelint',
 			'postcss-import',
 			'postcss-nested',
 			'postcss-custom-media',
@@ -21,5 +22,11 @@ module.exports = {
 		url:        'inline',
 		fallback:   'copy',
 		assetsPath: 'assets'
+	},
+	'stylelint': {
+		"extends": [
+			"stylelint-config-suitcss",
+			"./.stylelintrc"
+		]
 	}
 };
