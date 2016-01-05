6a5ad768e8351e44a99d8e891b0cfffbff2b6b95
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-05 01:40:44 -0800

    postcss-reporter

--- postcss.js
+++ postcss.js
@@ -1,22 +1,25 @@
-var plugins = [
-	'postcss-import',
-	'postcss-nested',
-	'postcss-custom-media',
-	'autoprefixer',
-	'postcss-color-rgba-fallback',
-	'postcss-url',
-	'postcss-cachebuster'
-];
-
-if (process.env.NODE_ENV === 'production') {
-	plugins.push('cssnano');
-}
+var _ = require('underscore');
 
 module.exports = {
-	'use':            plugins,
-	'cssnano':        { autoprefixer: false },
-	'postcss-import': { glob: true },
-	'postcss-url':    { url:        'inline',
-	                    fallback:   'copy',
-	                    assetsPath: 'assets' }
+	'use': _.union(
+		[
+			'postcss-import',
+			'postcss-nested',
+			'postcss-custom-media',
+			'autoprefixer',
+			'postcss-color-rgba-fallback',
+			'postcss-url',
+			'postcss-cachebuster'
+		],
+		(process.env.NODE_ENV === 'production') ? ['cssnano'] : [],
+		['postcss-reporter']
+	),
+	'cssnano':          { autoprefixer: false },
+	'postcss-import':   { glob: true },
+	'postcss-reporter': { clearMessages: true },
+	'postcss-url':      {
+		url:        'inline',
+		fallback:   'copy',
+		assetsPath: 'assets'
+	}
 };
