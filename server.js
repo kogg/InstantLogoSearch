9a36b41ec51a067b42c200dc340820bb868f02fd
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-04 23:29:29 -0800

    very simple in memory feathers service

--- server.js
+++ server.js
@@ -9,16 +9,25 @@ express_app.set('port', process.env.PORT || 5000);
 express_app.set('view engine', 'jade');
 express_app.set('views', path.join(__dirname, '/views'));
 
-express_app.get('/', function(req, res) {
-	var state = { messages: [
-		{ id: 0, text: 'hi wasup' },
-		{ id: 1, text: 'hey' },
-		{ id: 2, text: 'get to work' }
-	] };
+express_app.use('/api/messages', require('feathers-memory')());
 
-	res.render('main', {
-		markup: ReactDOM.renderToString(app(state)),
-		state:  state
+var i = 0;
+setInterval(function() {
+	express_app.service('api/messages').create({ text: 'this is message #' + i });
+	i++;
+}, 1000);
+
+express_app.get('/', function(req, res, next) {
+	express_app.service('api/messages').find(function(err, messages) {
+		if (err) {
+			return next(err);
+		}
+
+		var state = { messages: messages };
+		res.render('main', {
+			markup: ReactDOM.renderToString(app(state)),
+			state:  state
+		});
 	});
 });
 
