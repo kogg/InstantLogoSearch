f51fc62e0d273797ffdf72b70b9af2b2e9f5f4b6
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-07 20:03:32 -0800

    some reworking

--- webapp/store.js
+++ webapp/store.js
@@ -1,10 +1,12 @@
 var _           = require('underscore');
 var createStore = require('redux').createStore;
 
-var DevTools = process.env.DEVTOOLS && require('./components/DevTools');
+if (process.env.DEVTOOLS) {
+	createStore = require('./components/DevTools').instrument()(createStore);
+}
 
 module.exports = function(state) {
-	return (DevTools ? DevTools.instrument()(createStore) : createStore)(function(state, action) {
+	return createStore(function(state, action) {
 		switch (_.result(action, 'type')) {
 			case 'LOADED_MESSAGES':
 				return _.defaults({ messages: _.result(action, 'payload') }, state);
