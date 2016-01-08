cc6fe3b5b947a422a6d6df329bae4f08387feb98
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-07 20:51:09 -0800

    move some stuff from index to app

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var feathers   = require('feathers');
 var fs         = require('fs');
 var path       = require('path');
 
+var ReactDOM = require('react-dom/server');
+var Root     = require('./components/Root');
+var Store    = require('./store');
+
 var app = feathers();
 
 app.set('port', process.env.PORT || 5000);
@@ -15,9 +19,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, '/views'));
-
 app.locals.cacheBuster = function(assetPath) {
 	return assetPath + '?' + fs.statSync(path.join(__dirname, 'dist', assetPath)).mtime.getTime().toString(16);
 };
+
+// TODO Have legitimate endpoints
+app.use('/api/messages', require('feathers-memory')());
+var i = 0;
+setInterval(function() {
+	app.service('api/messages').create({ text: 'this is message #' + i });
+	i++;
+}, 1000);
+
+app.get('/', function(req, res) {
+	var store = Store();
+
+	res.render('main', {
+		markup: ReactDOM.renderToString(Root(store)),
+		state:  store.getState()
+	});
+});
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 require('babel-register');
-var ReactDOM = require('react-dom/server');
 
-var app   = require('./app');
-var Root  = require('./components/Root');
-var Store = require('./store');
-
-// TODO Have legitimate endpoints
-app.use('/api/messages', require('feathers-memory')());
-var i = 0;
-setInterval(function() {
-	app.service('api/messages').create({ text: 'this is message #' + i });
-	i++;
-}, 1000);
-
-app.get('/', function(req, res) {
-	var store = Store();
-
-	res.render('main', {
-		markup: ReactDOM.renderToString(Root(store)),
-		state:  store.getState()
-	});
-});
+var app = require('./app');
 
 app.listen(app.get('port'));
