3da4df5a3bb08f3d318115d3a8f12f5b3c653ad3
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-13 02:08:08 -0500

    better feathers-redux reducer controls

--- browser.js
+++ browser.js
@@ -1,12 +1,7 @@
-/* global io */
-var feathers = require('feathers-client');
 var ReactDOM = require('react-dom');
 
 var Root  = require('./components/Root');
 var Store = require('./store');
-
-var app = feathers();
-app.configure(feathers.socketio(io()));
 
 var store = Store(JSON.parse(document.getElementById('react-state').innerHTML));
 
--- feathers-redux.js
+++ feathers-redux.js
@@ -1,8 +1,78 @@
 var _               = require('underscore');
 var combineReducers = require('redux').combineReducers;
 
-module.exports = function(resource, state) {
+var resourceReducer = function(resource, state) {
 	var RESOURCE = resource.toUpperCase();
+
+	return combineReducers({
+		loading: function(state, action) {
+			switch (action.type) {
+				case 'LOADING_' + RESOURCE:
+					return true;
+				case 'LOADED_' + RESOURCE:
+					return false;
+				default:
+					return state || false;
+			}
+		},
+		updating: function(state, action) {
+			switch (action.type) {
+				case 'UDPATING_' + RESOURCE:
+				case 'PATCHING_' + RESOURCE:
+					return true;
+				case 'UDPATED_' + RESOURCE:
+				case 'PATCHED_' + RESOURCE:
+					return false;
+				default:
+					return state || false;
+			}
+		},
+		removing: function(state, action) {
+			switch (action.type) {
+				case 'REMOVING_' + RESOURCE:
+					return true;
+				case 'REMOVED_' + RESOURCE:
+					return false;
+				default:
+					return state || false;
+			}
+		},
+		error: function(state, action) {
+			switch (action.type) {
+				case 'LOADING_' + RESOURCE:
+				case 'UDPATING_' + RESOURCE:
+				case 'PATCHING_' + RESOURCE:
+				case 'REMOVING_' + RESOURCE:
+					return null;
+				case 'LOADED_' + RESOURCE:
+				case 'UDPATED_' + RESOURCE:
+				case 'PATCHED_' + RESOURCE:
+				case 'REMOVED_' + RESOURCE:
+					return action.error && action.payload;
+				default:
+					return state || null;
+			}
+		},
+		data: function(state, action) {
+			switch (action.type) {
+				case 'LOADED_' + RESOURCE:
+				case 'CREATED_' + RESOURCE:
+				case 'UDPATED_' + RESOURCE:
+				case 'PATCHED_' + RESOURCE:
+					return action.error ? state : action.payload;
+				case 'REMOVED_' + RESOURCE:
+					return null;
+				default:
+					return state || {};
+			}
+		}
+	}, state);
+};
+
+var resourcesReducer = function(resource, state) {
+	var RESOURCE = resource.toUpperCase();
+
+	var singleItemReducer = resourceReducer(resource, {});
 
 	return combineReducers({
 		loading: function(state, action) {
@@ -15,11 +85,23 @@ module.exports = function(resource, state) {
 					return state || false;
 			}
 		},
+		creating: function(state, action) {
+			switch (action.type) {
+				case 'CREATING_' + RESOURCE:
+					return true;
+				case 'CREATED_' + RESOURCE:
+					return false;
+				default:
+					return state || false;
+			}
+		},
 		error: function(state, action) {
 			switch (action.type) {
 				case 'LOADING_' + RESOURCE + 'S':
+				case 'CREATING_' + RESOURCE:
 					return null;
 				case 'LOADED_' + RESOURCE + 'S':
+				case 'CREATED_' + RESOURCE:
 					return action.error && action.payload;
 				default:
 					return state || null;
@@ -29,26 +111,28 @@ module.exports = function(resource, state) {
 			switch (action.type) {
 				case 'LOADED_' + RESOURCE + 'S':
 					return action.error ? state : _.indexBy(action.payload, 'id');
-				case 'LOADING_' + RESOURCE:
-				case 'CREATING_' + RESOURCE:
-				case 'UDPATING_' + RESOURCE:
-				case 'PATCHING_' + RESOURCE:
+				case 'REMOVED_' + RESOURCE:
 					state = _.clone(state);
-					state[action.payload.id] = _.defaults({ loading: true, error: null }, state[action.payload.id]);
-					return state;
-				case 'LOADED_' + RESOURCE:
-				case 'CREATED_' + RESOURCE:
-				case 'UDPATED_' + RESOURCE:
-				case 'PATCHED_' + RESOURCE:
-					state = _.clone(state);
-					state[action.payload.id] = _.defaults(
-						{ loading: false, error: action.error && action.payload },
-						action.error ? state[action.payload.id] : action.payload
-					);
+					if (action.error) {
+						state[action.payload.id] = singleItemReducer(state[action.payload.id], action);
+					} else {
+						delete state[action.payload.id];
+					}
 					return state;
 				default:
-					return state || {};
+					if (!action.payload || !action.payload.id) {
+						return state || {};
+					}
+					state = _.clone(state);
+					state[action.payload.id] = singleItemReducer(state[action.payload.id], action);
+					return state;
 			}
 		}
 	}, state);
 };
+
+module.exports = function(resource, options, state) {
+	return options.id ? resourceReducer(resource, state) : resourcesReducer(resource, state);
+};
+module.exports.resourceReducer  = resourceReducer;
+module.exports.resourcesReducer = resourcesReducer;
