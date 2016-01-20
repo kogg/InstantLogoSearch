0625f6806692993b5e8bc2e67b0e5250465e8dba
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-20 12:01:34 -0800

    use serverActionsReducer from feathers-react-redux

--- store.js
+++ store.js
@@ -1,9 +1,10 @@
-var _                = require('underscore');
-var applyMiddleware  = require('redux').applyMiddleware;
-var combineReducers  = require('redux').combineReducers;
-var createStore      = require('redux').createStore;
-var resourcesReducer = require('feathers-react-redux').resourcesReducer;
-var thunkMiddlware   = require('redux-thunk');
+var _                    = require('underscore');
+var applyMiddleware      = require('redux').applyMiddleware;
+var combineReducers      = require('redux').combineReducers;
+var createStore          = require('redux').createStore;
+var resourcesReducer     = require('feathers-react-redux').resourcesReducer;
+var serverActionsReducer = require('feathers-react-redux').serverActionsReducer;
+var thunkMiddlware       = require('redux-thunk');
 
 if (process.env.DEVTOOLS) {
 	createStore = require('./components/DevTools').instrument()(createStore);
@@ -11,17 +12,6 @@ if (process.env.DEVTOOLS) {
 createStore = applyMiddleware(thunkMiddlware)(createStore);
 
 module.exports = _.partial(createStore, combineReducers({
-	server_actions: function(state, action) {
-		switch (action.type) {
-			case 'SERVER_ACTION':
-				return _.chain(state)
-					.union([action.payload])
-					// TODO Unique based on something more granular
-					.uniq(false, 'type')
-					.value();
-			default:
-				return state || [];
-		}
-	},
-	messages: resourcesReducer('message')
+	server_actions: serverActionsReducer,
+	messages:       resourcesReducer('message')
 }));
