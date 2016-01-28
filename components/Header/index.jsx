2e953f4c490a8cf8fb9154ee3c9499efdbdb531a
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-28 12:46:20 -0800

    expandable header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,11 +1,20 @@
 var React      = require('react');
 
 module.exports = React.createClass({
+	getInitialState: function() {
+		return {
+			expanded: false,
+			filters:  []
+		};
+	},
 	render: function() {
 		return (
-			<div className="header">
+			<div className={'header' + (this.state.expanded ? ' header_expanded' : '')}>
 				<input ref="search" type="text" autoFocus onChange={function() {
-					this.setState({ filters: this.refs.search.value.toLowerCase().split(/\s+/) });
+					this.setState({
+						expanded: true,
+						filters:  this.refs.search.value.toLowerCase().split(/\s+/)
+					});
 				}.bind(this)} />
 			</div>
 		);
