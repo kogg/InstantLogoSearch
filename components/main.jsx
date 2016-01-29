4fc894435235eec4c81076c794ed72f471883f86
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-29 02:03:18 -0800

    using react instead of jade

---
+++ components/main.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function() {
+		return (
+			<html>
+				<head>
+					<title>Base App</title>
+					<meta charSet="utf-8" />
+					<link rel="stylesheet" type="text/css" href={this.props.cacheBuster('css/main.css')} />
+				</head>
+				<body>
+					<div id="react-app" dangerouslySetInnerHTML={{ __html: this.props.markup }} />
+					<script id="react-state" type="text/json" dangerouslySetInnerHTML={{ __html: JSON.stringify(this.props.state) }} />
+					<script type="text/javascript" src="/socket.io/socket.io.js" />
+					<script type="text/javascript" src={this.props.cacheBuster('js/main.js')} async />
+				</body>
+			</html>
+		);
+	}
+});
