03f46d6ac82904d002d959b577c038f7125d80a4
Cameron Rohani <cameron.rohani@gmail.com>
2016-01-29 15:09:44 -0800

    search logos wip

--- components/Logos/index.jsx
+++ components/Logos/index.jsx
@@ -15,26 +15,39 @@ module.exports = React.createClass({
 			<div className="logos">
 				<div className="logos-container">
 					<div className="logos-title">
-						<h3>Most Popular</h3>
+						<h3>Most Popular Logos</h3>
 					</div>
-					<ul>
+					<ul className="flex-grid">
 						{logos.map(function(logo) {
 							return (
-								<li key={logo.id}>
-									{logo.name}
-									<a href={logo.svg_url} download={logo.name + '.svg'}> [Download SVG]</a>
-									<a href={logo.png_url} download={logo.name + '.png'}> [Download PNG]</a>
-									{
-										logo.in_collection ?
-											<a href="" onClick={function(e) {
-												e.preventDefault();
-												this.props.onUncollectLogo(logo);
-											}.bind(this)}> [Remove from Collection]</a> :
-											<a href="" onClick={function(e) {
-												e.preventDefault();
-												this.props.onCollectLogo(logo);
-											}.bind(this)}> [Add to Collection]</a>
-									}
+								<li className="brand-logo" key={logo.id}>
+									<div className="brand-logo-image">
+										<img src={logo.svg_url} />
+									</div>
+									<div className="brand-logo-ctas">
+										<div>
+											<strong>{logo.name}</strong>
+										</div>
+										<div>
+											<a href={logo.svg_url} download={logo.name + '.svg'}> [Download SVG]</a>
+										</div>
+										<div>
+											<a href={logo.png_url} download={logo.name + '.png'}> [Download PNG]</a>
+										</div>
+										<div>
+											{
+												logo.in_collection ?
+													<a href="" onClick={function(e) {
+														e.preventDefault();
+														this.props.onUncollectLogo(logo);
+													}.bind(this)}> [Remove from Collection]</a> :
+													<a href="" onClick={function(e) {
+														e.preventDefault();
+														this.props.onCollectLogo(logo);
+													}.bind(this)}> [Add to Collection]</a>
+											}
+										</div>
+									</div>
 								</li>
 							);
 						}.bind(this))}
