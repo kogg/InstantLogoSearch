c0c5b1aecd10c3b54485799533c4f24ea6cc8b76
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-29 03:27:01 -0800

    Using react-helmet for some meta tags

--- components/App/index.jsx
+++ components/App/index.jsx
@@ -7,8 +7,17 @@ module.exports = React.createClass({
 	render: function() {
 		return (
 			<div>
-				{/* https://github.com/nfl/react-helmet */}
-				<Helmet title="Boilerplate" />
+				<Helmet
+					title={process.env.npm_package_title}
+					meta={[
+						{ name: 'description', content: process.env.npm_package_description },
+						{ property: 'og:site_name', content: process.env.npm_package_title },
+						{ property: 'og:title', content: process.env.npm_package_title },
+						{ property: 'og:description', content: process.env.npm_package_description },
+						{ name: 'twitter:title', content: process.env.npm_package_title },
+						{ name: 'twitter:description', content: process.env.npm_package_description }
+					]}
+				/>
 				<Messages />
 			</div>
 		);
