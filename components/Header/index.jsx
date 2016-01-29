7c0f61d4ca65f1653e67efa61549577943992818
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-29 15:11:53 -0800

    debouncing is my jams

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,3 +1,4 @@
+var _          = require('underscore');
 var classNames = require('classnames');
 var React      = require('react');
 
@@ -18,10 +19,10 @@ module.exports = React.createClass({
 					<h2>{process.env.npm_package_description}</h2>
 					<div className="search-container">
 						<form>
-							<input className="search-input" placeholder="What logo are you looking for?" ref="search" type="text" autoFocus onChange={function() {
+							<input className="search-input" placeholder="What logo are you looking for?" ref="search" type="text" autoFocus onChange={_.debounce(function() {
 								this.setState({ expanded: true });
 								this.props.onFilter(this.refs.search.value.toLowerCase().split(/\s+/));
-							}.bind(this)} />
+							}.bind(this), 25)} />
 							<i className="search-icon"></i>
 						</form>
 					</div>
