cfa13be53a3d65c20a7ee7a058cffd7777e9cfc2
Cameron Rohani <cameron.rohani@gmail.com>
2016-01-29 13:51:49 -0800

    header wip

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -13,6 +13,7 @@ module.exports = React.createClass({
 
 		return (
 			<div className={headerClasses}>
+				<div className="logo"></div>
 				<div className="header-content">
 					<h1>Instant Logo Search</h1>
 					<h2>Search and download thousands of logos  svg or png</h2>
--- components/Logos/index.jsx
+++ components/Logos/index.jsx
@@ -12,28 +12,35 @@ module.exports = React.createClass({
 		}.bind(this));
 
 		return (
-			<ul className="logos">
-				{logos.map(function(logo) {
-					return (
-						<li key={logo.id}>
-							{logo.name}
-							<a href={logo.svg_url} download={logo.name + '.svg'}> [Download SVG]</a>
-							<a href={logo.png_url} download={logo.name + '.png'}> [Download PNG]</a>
-							{
-								logo.in_collection ?
-									<a href="" onClick={function(e) {
-										e.preventDefault();
-										this.props.onUncollectLogo(logo);
-									}.bind(this)}> [Remove from Collection]</a> :
-									<a href="" onClick={function(e) {
-										e.preventDefault();
-										this.props.onCollectLogo(logo);
-									}.bind(this)}> [Add to Collection]</a>
-							}
-						</li>
-					);
-				}.bind(this))}
-			</ul>
+			<div className="logos">
+				<div className="logos-container">
+					<div className="logos-title">
+						<h3>Most Popular</h3>
+					</div>
+					<ul>
+						{logos.map(function(logo) {
+							return (
+								<li key={logo.id}>
+									{logo.name}
+									<a href={logo.svg_url} download={logo.name + '.svg'}> [Download SVG]</a>
+									<a href={logo.png_url} download={logo.name + '.png'}> [Download PNG]</a>
+									{
+										logo.in_collection ?
+											<a href="" onClick={function(e) {
+												e.preventDefault();
+												this.props.onUncollectLogo(logo);
+											}.bind(this)}> [Remove from Collection]</a> :
+											<a href="" onClick={function(e) {
+												e.preventDefault();
+												this.props.onCollectLogo(logo);
+											}.bind(this)}> [Add to Collection]</a>
+									}
+								</li>
+							);
+						}.bind(this))}
+					</ul>
+				</div>
+			</div>
 		);
 	}
 });
