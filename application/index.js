0087a2aecccaf6cb73ac5350c4235987653fa93e
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-30 01:55:21 -0800

    have the app's catch all routes at the very VERY end

--- application/index.js
+++ application/index.js
@@ -1,10 +1,8 @@
 var _            = require('underscore');
 var bodyParser   = require('body-parser');
 var compression  = require('compression');
-var error        = require('debug')(process.env.npm_package_name + ':application:error');
 var feathers     = require('feathers');
 var helmet       = require('helmet');
-var http         = require('http');
 var path         = require('path');
 var serverRender = require('feathers-react-redux/serverRender');
 
@@ -39,17 +37,6 @@ app.get('/', function(req, res, next) {
 		.then(function(locals) {
 			res.render('index', _.extend(locals, { state: store.getState() }));
 		});
-});
-
-app.all('*', function(req, res, next) {
-	var err = new Error(http.STATUS_CODES[404]);
-	err.status = 404;
-	next(err);
-});
-
-app.use(function(err, req, res, next) {
-	error('error on url ' + req.url, err);
-	next(err);
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 require('babel-register');
 var debug  = require('debug')(process.env.npm_package_name + ':application');
+var error  = require('debug')(process.env.npm_package_name + ':application:error');
+var http   = require('http');
 var memory = require('feathers-memory');
 var os     = require('os');
 
 var app = require('./application');
 
 app.use('/api/messages', memory());
+
+app.all('*', function(req, res, next) {
+	var err = new Error(http.STATUS_CODES[404]);
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next) {
+	error('error on url ' + req.url, err);
+	next(err);
+});
 
 app.listen(app.get('port'), function() {
 	debug('Server running', 'http://' + os.hostname() + ':' + app.get('port'));
