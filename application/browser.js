8ed246fb0715e0d6045990b993b5930fe7cfc2d0
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-03 14:48:48 -0800

    works with or without sockets

--- application/browser.js
+++ application/browser.js
@@ -1,10 +1,14 @@
 var feathers = require('feathers-client');
 
-var app = feathers();
+var app = feathers(document.location.origin);
 if (process.env.npm_package_feathersjs_socket) {
 	/* global io */
 	app.configure(feathers.socketio(io()));
 	/* global io:false */
+} else {
+	global.fetch = null;
+	require('whatwg-fetch');
+	app.configure(feathers.fetch(global.fetch));
 }
 
 module.exports = app;
