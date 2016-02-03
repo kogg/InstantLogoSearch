4e6589e186650b8e69dbc4d6304a345d23c7bd89
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-03 03:09:21 -0800

    cacheBuster is a static method

--- components/index.jsx
+++ components/index.jsx
@@ -2,9 +2,11 @@ var fs    = require('fs');
 var path  = require('path');
 var React = require('react');
 
-module.exports = React.createClass({
-	cacheBuster: function(assetPath) {
-		return assetPath + '?' + fs.statSync(path.join(__dirname, '../dist', assetPath)).mtime.getTime().toString(16);
+var HTMLDoc = React.createClass({
+	statics: {
+		cacheBuster: function(assetPath) {
+			return assetPath + '?' + fs.statSync(path.join(__dirname, '../dist', assetPath)).mtime.getTime().toString(16);
+		}
 	},
 	render: function() {
 		return (
@@ -14,7 +16,7 @@ module.exports = React.createClass({
 					{this.props.head.title.toComponent()}
 					<meta charSet="utf-8" />
 					{this.props.head.meta.toComponent()}
-					<link rel="stylesheet" type="text/css" href={this.cacheBuster('css/main.css')} />
+					<link rel="stylesheet" type="text/css" href={HTMLDoc.cacheBuster('css/main.css')} />
 					{this.props.head.link.toComponent()}
 					{process.env.GOOGLE_ANALYTICS_ID && <script dangerouslySetInnerHTML={{
 						__html: '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\n' +
@@ -28,10 +30,12 @@ module.exports = React.createClass({
 					<div id="react-app" dangerouslySetInnerHTML={{ __html: this.props.markup }} />
 					<script id="react-state" type="text/json" dangerouslySetInnerHTML={{ __html: JSON.stringify(this.props.state) }} />
 					{process.env.npm_package_feathersjs_socket && <script type="text/javascript" src="/socket.io/socket.io.js" />}
-					<script type="text/javascript" src={this.cacheBuster('js/main.js')} async />
+					<script type="text/javascript" src={HTMLDoc.cacheBuster('js/main.js')} async />
 					{this.props.head.script.toComponent()}
 				</body>
 			</html>
 		);
 	}
 });
+
+module.exports = HTMLDoc;
