2864558b44c25cbd6bb9fa422e70f7946ab46501
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-04 02:04:26 -0800

    Lol

--- components/routes.jsx
+++ components/routes.jsx
@@ -2,8 +2,8 @@ var IndexRoute = require('react-router').IndexRoute;
 var React      = require('react');
 var Route      = require('react-router').Route;
 
-var App      = require('../components/App');
-var Messages = require('../components/Messages');
+var App      = require('./App');
+var Messages = require('./Messages');
 
 module.exports = (
 	<Route path="/" component={App}>
