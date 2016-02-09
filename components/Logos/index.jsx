bc623e46599a4cb5516a6c78744360d066f722e9
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-09 15:05:42 -0800

    Moving which Logos are used up to MainPage

--- components/Logos/index.jsx
+++ components/Logos/index.jsx
@@ -1,10 +1,8 @@
 var _                        = require('underscore');
 var classNames               = require('classnames');
 var connect                  = require('react-redux').connect;
-var createSelector           = require('reselect').createSelector;
 var createStructuredSelector = require('reselect').createStructuredSelector;
 var error                    = require('debug')(process.env.npm_package_name + ':application:error');
-var levenshtein              = require('fast-levenshtein');
 var React                    = require('react');
 
 var actions = require('../../actions');
@@ -12,44 +10,6 @@ var actions = require('../../actions');
 var PAGE_SIZE = 20;
 
 module.exports = connect(createStructuredSelector({
-	logos: createSelector(
-		_.property('logos'),
-		createSelector(
-			_.property('searching'),
-			function(searching) {
-				return _.chain(searching.trim().toLowerCase().replace(/[.\-()]/gi, '').split(/\s+/))
-					.compact()
-					.sortBy(function(filter) {
-						return -filter.length; // The longer the word, the more likely it won't match anything
-					})
-					.value();
-			}
-		),
-		function(logos, filters) {
-			if (_.isEmpty(filters)) {
-				return _.chain(logos)
-					.sortBy(function(logo) {
-						return -logo.data.downloads;
-					})
-					.pluck('data')
-					.value();
-			}
-			return _.chain(logos)
-				.reject(function(logo) {
-					return _.some(filters, function(filter) {
-						return logo.data.shortname.indexOf(filter) === -1;
-					});
-				})
-				.sortBy(function(logo) {
-					return _.chain(filters)
-						.map(_.partial(levenshtein.get, logo.data.shortname))
-						.max()
-						.value();
-				})
-				.pluck('data')
-				.value();
-		}
-	),
 	collection:  _.property('collection'),
 	considering: _.property('considering'),
 	searching:   _.property('searching')
--- components/MainPage/index.jsx
+++ components/MainPage/index.jsx
@@ -1,10 +1,53 @@
-var connect = require('react-redux').connect;
-var React   = require('react');
+var _                        = require('underscore');
+var connect                  = require('react-redux').connect;
+var createSelector           = require('reselect').createSelector;
+var createStructuredSelector = require('reselect').createStructuredSelector;
+var levenshtein              = require('fast-levenshtein');
+var React                    = require('react');
 
 var Logos = require('../Logos');
 
-module.exports = connect()(React.createClass({
+module.exports = connect(createStructuredSelector({
+	logos: createSelector(
+		_.property('logos'),
+		createSelector(
+			_.property('searching'),
+			function(searching) {
+				return _.chain(searching.trim().toLowerCase().replace(/[.\-()]/gi, '').split(/\s+/))
+					.compact()
+					.sortBy(function(filter) {
+						return -filter.length; // The longer the word, the more likely it won't match anything
+					})
+					.value();
+			}
+		),
+		function(logos, filters) {
+			if (_.isEmpty(filters)) {
+				return _.chain(logos)
+					.sortBy(function(logo) {
+						return -logo.data.downloads;
+					})
+					.pluck('data')
+					.value();
+			}
+			return _.chain(logos)
+				.reject(function(logo) {
+					return _.some(filters, function(filter) {
+						return logo.data.shortname.indexOf(filter) === -1;
+					});
+				})
+				.sortBy(function(logo) {
+					return _.chain(filters)
+						.map(_.partial(levenshtein.get, logo.data.shortname))
+						.max()
+						.value();
+				})
+				.pluck('data')
+				.value();
+		}
+	)
+}))(React.createClass({
 	render: function() {
-		return <Logos history={this.props.history} />;
+		return <Logos logos={this.props.logos} history={this.props.history} />;
 	}
 }));
