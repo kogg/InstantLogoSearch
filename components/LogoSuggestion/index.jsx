f353f4c92634781fc94440b94eb0fd6d94ccefad
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-11 19:52:39 -0800

    LogoSuggestion uses BEM now

--- components/LogoSuggestion/index.jsx
+++ components/LogoSuggestion/index.jsx
@@ -13,15 +13,15 @@ module.exports = React.createClass({
 	},
 	render: function() {
 		return (
-			<li className="brand-logo brand-logo_missing">
-				<div className="brand-logo-image"></div>
+			<li className="brand-logo suggestion">
+				<div className="brand-logo-image suggestion__image"></div>
 				{this.state.popover && (
-					<div className="pop-over">
+					<div className="suggestion__popover">
 						{!this.state.success && !this.state.failure && (
-							<div className="suggest">
+							<div>
 								<strong>Suggest or Upload a logo</strong>
 								<p>What logo or logo variation were you wanting?</p>
-								<form className="submit-logo-form" onSubmit={function(e) {
+								<form className="suggestion__form" onSubmit={function(e) {
 									e.preventDefault();
 									this.suggestLogo(this.refs.suggest_name.value, this.refs.suggest_file.files[0]).then(
 										function() {
@@ -32,31 +32,31 @@ module.exports = React.createClass({
 										}.bind(this)
 									);
 								}.bind(this)}>
-									<input className="submit-logo-form__input" placeholder="i.e: facebook circle" type="text" ref="suggest_name" defaultValue={this.props.value}/>
-									<label htmlFor="file-upload" className="custom-file-upload">Upload SVG*</label>
-									<input className="file-upload" id="file-upload" type="file" ref="suggest_file" accept=".svg" />
-									<input className="submit-logo submit-logo-success" type="submit" />
-									<span className="footnote">*Optional but appreciated 😇</span>
+									<input className="suggestion__input" placeholder="i.e: facebook circle" type="text" ref="suggest_name" defaultValue={this.props.value}/>
+									<label className="suggestion__upload-label" htmlFor="file-upload">Upload SVG*</label>
+									<input className="suggestion__upload-input" id="file-upload" type="file" ref="suggest_file" accept=".svg" />
+									<input className="suggestion__submit suggestion__submit_success" type="submit" />
+									<span className="suggestion__footnote">*Optional but appreciated 😇</span>
 								</form>
 							</div>
 						)}
 						{this.state.success && (
-							<div className="success">
+							<div>
 								<strong>Success!</strong>
 								<p>Thank you so much! We will quickly review your request and try to get something up later today!</p>
 								<span className="emoji">😍</span>
-								<a className="another-one" onClick={function(e) {
+								<a className="suggestion__another-one" onClick={function(e) {
 									e.preventDefault();
 									this.setState({ success: null });
 								}.bind(this)}>submit another one</a>
 							</div>
 						)}
 						{this.state.failure && (
-							<div className="error">
+							<div>
 								<strong>Error! Error!</strong>
 								<p>We're sorry but something went terribly wrong! {this.state.failure.message}</p>
 								<span className="emoji">😓</span>
-								<a className="another-one" onClick={function(e) {
+								<a className="suggestion__another-one" onClick={function(e) {
 									e.preventDefault();
 									this.setState({ failure: null });
 								}.bind(this)}>let's try again!</a>
@@ -65,14 +65,14 @@ module.exports = React.createClass({
 					</div>
 				)}
 				<div className="flex-center">
-					<div className="">
+					<div>
 						<span>Can't find quite what you're looking for? </span>
-						<u className="activate-pop-over" onClick={function(e) {
+						<u className="suggestion__activate-popover" onClick={function(e) {
 							e.preventDefault();
 							this.setState({ popover: true });
 						}.bind(this)}>Suggest</u>
 						<span> a logo or </span>
-						<u className="activate-pop-over" onClick={function(e) {
+						<u className="suggestion__activate-popover" onClick={function(e) {
 							e.preventDefault();
 							this.setState({ popover: true });
 						}.bind(this)}>upload</u>
