45fc065d7e10e39b7776eff37c2b0671eccc249e
Cameron Rohani <cameron.rohani@gmail.com>
2016-02-15 17:49:11 -0800

    small ui changes

--- components/ShareButtons/index.jsx
+++ components/ShareButtons/index.jsx
@@ -15,17 +15,23 @@ module.exports = React.createClass({
 					}
 					onClick={function() {
 						ga('send', 'social', 'Twitter', 'Share', process.env.npm_package_homepage);
-					}}>tweet</a>
+					}}>
+					<span>tweet</span>
+				</a>
 				<a className="social-action social-action_facebook" target="_blank"
 					href={'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(process.env.npm_package_homepage)}
 					onClick={function() {
 						ga('send', 'social', 'Facebook', 'Share', process.env.npm_package_homepage);
-					}}>share</a>
+					}}>
+					<span>share</span>
+				</a>
 				<a className="social-action social-action_github" target="_blank"
 					href="https://github.com/kogg/instant-logos/stargazers"
 					onClick={function() {
 						ga('send', 'social', 'GitHub', 'Star', 'https://github.com/kogg/instant-logos/stargazers');
-					}}>star</a>
+					}}>
+					<span>star</span>
+				</a>
 			</div>
 		);
 	}
