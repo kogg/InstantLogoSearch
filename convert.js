7ca568b7b5e6f2a13bf785dbfd7c5ce33f838b38
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-21 20:48:03 -0800

    when svg2png fails, specify both dimensions instead of just the height

--- convert.js
+++ convert.js
@@ -1,9 +1,12 @@
-var _         = require('underscore');
 var fs        = require('fs');
 var memoize   = require('memoizee');
 var promisify = require('es6-promisify');
 var svg2png   = require('svg2png');
 
 module.exports = memoize(function(file_path) {
-	return promisify(fs.readFile)(file_path).then(_.partial(svg2png, _, { height: 512 }));
+	return promisify(fs.readFile)(file_path).then(function(data) {
+		return svg2png(data, { height: 512 }).catch(function() {
+			return svg2png(data, { height: 512, width: 1024 });
+		});
+	});
 }, { maxAge: 30 * 60 * 1000, preFetch: true });
