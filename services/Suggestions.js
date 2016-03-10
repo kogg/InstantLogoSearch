6b700bab1f7ad18cf696dcf51dd0f18c4bd5e8ec
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-03-10 08:00:43 -0800

    Upload text files to dropbox on suggestion without file #36

--- services/Suggestions.js
+++ services/Suggestions.js
@@ -1,4 +1,44 @@
 var _         = require('underscore');
+var http      = require('http');
+var promisify = require('es6-promisify');
+var request   = promisify(require('request'));
+
+function requestPromise(options) {
+	return request(options).then(function(results) {
+		if (results[0].statusCode >= 300) {
+			throw new Error(results[0].statusCode + ': ' + (_.result(results[1], 'error_description') || _.result(results[0], 'statusMessage')));
+		}
+		return results;
+	});
+}
+
+module.exports = {
+	create: function(data) {
+		if (!_.result(data, 'name')) {
+			var err = new Error(http.STATUS_CODES[400]);
+			err.status = 400;
+			return Promise.reject(err);
+		}
+		return requestPromise({
+			url:     'https://content.dropboxapi.com/2/files/upload',
+			method:  'POST',
+			headers: {
+				'Authorization':   'Bearer ' + process.env.DROPBOX_ACCESS_TOKEN,
+				'Content-Type':    'application/octet-stream',
+				'Dropbox-API-Arg': JSON.stringify({
+					path:       '/' + data.name.replace(/\//g, '-') + '.txt',
+					mode:       'add',
+					autorename: true,
+					mute:       false
+				})
+			}
+		}).then(function(response) {
+			return _.pick(JSON.parse(response[1]), 'id');
+		});
+	}
+};
+/*
+var _         = require('underscore');
 var fs        = require('fs');
 var http      = require('http');
 var memoize   = require('memoizee');
@@ -206,3 +246,4 @@ module.exports = {
 			});
 	}
 };
+*/
