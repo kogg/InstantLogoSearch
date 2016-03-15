84285b276b326b80afd345f7495cf9c405a0b23d
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-03-14 17:31:14 -0700

    No more CACHE_LOG

--- convert.js
+++ convert.js
@@ -1,13 +1,7 @@
-var _         = require('underscore');
 var fs        = require('fs');
 var memoize   = require('memoizee');
 var promisify = require('es6-promisify');
 var svg2png   = require('svg2png');
-
-if (process.env.CACHE_LOG) {
-	var memProfile = require('memoizee/profile');
-	setInterval(_.compose(console.log, memProfile.log), process.env.CACHE_LOG);
-}
 
 module.exports = memoize(function(file_path) {
 	return promisify(fs.readFile)(file_path).then(function(data) {
