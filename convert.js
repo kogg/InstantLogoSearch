52a1024cd8a39f555cbb57e435df7ceaebe518ab
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-03-14 17:36:53 -0700

    DONT_CACHE_PNGS var

--- convert.js
+++ convert.js
@@ -3,10 +3,14 @@ var memoize   = require('memoizee');
 var promisify = require('es6-promisify');
 var svg2png   = require('svg2png');
 
-module.exports = memoize(function(file_path) {
+module.exports = function(file_path) {
 	return promisify(fs.readFile)(file_path).then(function(data) {
 		return svg2png(data, { height: 512 }).catch(function() {
 			return svg2png(data, { height: 512, width: 1024 });
 		});
 	});
-}, { maxAge: 10 * 60 * 1000, preFetch: true });
+};
+
+if (!process.env.DONT_CACHE_PNGS) {
+	module.exports = memoize(module.exports, { maxAge: 10 * 60 * 1000, preFetch: true });
+}
