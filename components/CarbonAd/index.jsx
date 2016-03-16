af6b40bc3c7e899d698d55137505396d0a352901
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-03-16 16:01:26 -0700

    Show the CarbonAd again

--- components/CarbonAd/index.jsx
+++ components/CarbonAd/index.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
 	render: function() {
 		return (
-			<li id="carbonads" className="ad brand-logo ">
+			<li id="carbonads" className="ad brand-logo">
 				<span>
 					<span className="carbon-wrap">
 						<a href="" className="carbon-img" target="_blank">
@@ -12,7 +12,7 @@ module.exports = React.createClass({
 						<a href="" className="carbon-text" target="_blank">Slack - its teamwork, but simpler, more pleasant, and more productive.</a>
 					</span>
 					<a href="http://carbonads.net/" className="carbon-poweredby" target="_blank">ads via Carbon</a>
-						<img src="https://slack.sp1.convertro.com/view/vt/v1/slack/0/cvo.gif?cvosrc=display.carbon.plaid%20design&amp;utm_source=carbon&amp;utm_medium=display&amp;utm_campaign=plaid%20design" border="0" height="1" width="1" />
+					<img src="https://slack.sp1.convertro.com/view/vt/v1/slack/0/cvo.gif?cvosrc=display.carbon.plaid%20design&amp;utm_source=carbon&amp;utm_medium=display&amp;utm_campaign=plaid%20design" border="0" height="1" width="1" />
 				</span>
 			</li>
 		);
