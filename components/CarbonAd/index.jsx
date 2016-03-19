d5f94c63a3f0b59d54dc9bb51a815ffa3b6d4944
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-03-18 19:10:48 -0700

    Reattach carbonads anywhere it needs to be #46

--- components/CarbonAd/index.jsx
+++ components/CarbonAd/index.jsx
@@ -1,11 +1,23 @@
 var React = require('react');
 
-module.exports = React.createClass({
+var CarbonAd = module.exports = React.createClass({
+	statics: {
+		carbonadsDom: null
+	},
 	render: function() {
 		return (
 			<li className="ad brand-logo">
-				<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&serve=CVYD42T&placement=instantlogosearchcom" id="_carbonads_js"></script>
+				<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1696&serve=CVYD42T&placement=instantlogosearchcom" id="_carbonads_js" ref="carbonadsjs"></script>
 			</li>
 		);
+	},
+	componentDidMount: function() {
+		if (CarbonAd.carbonadsDom) {
+			this.refs.carbonadsjs.parentNode.insertBefore(CarbonAd.carbonadsDom, this.refs.carbonadsjs.nextSibling);
+			CarbonAd.carbonadsDom = null;
+		}
+	},
+	componentWillUnmount: function() {
+		CarbonAd.carbonadsDom = document.getElementById('carbonads');
 	}
 });
