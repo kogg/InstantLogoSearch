d74d35ee7ef3bbaf52d862c2416b2c508647f310
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-03-31 01:03:44 -0700

    Better rollbar config

--- rollbar/client.js
+++ rollbar/client.js
@@ -1,11 +1,18 @@
 var Rollbar = require('rollbar');
 
 module.exports = Rollbar.init({
-	accessToken:     process.env.ROLLBAR_CLIENT_ACCESS_TOKEN,
-	captureUncaught: true,
-	payload:         {
+	payload: {
+		client: {
+			javascript: {
+				code_version:          process.env.npm_package_gitHead,
+				source_map_enabled:    true,
+				guess_uncaught_frames: true
+			}
+		},
 		environment: process.env.ROLLBAR_ENV || process.env.NODE_ENV
 	},
-	enabled: process.env.NODE_ENV && process.env.ROLLBAR_CLIENT_ACCESS_TOKEN,
-	verbose: !process.env.NODE_ENV
+	accessToken:     process.env.ROLLBAR_CLIENT_ACCESS_TOKEN,
+	captureUncaught: true,
+	enabled:         process.env.NODE_ENV && process.env.ROLLBAR_CLIENT_ACCESS_TOKEN,
+	verbose:         !process.env.NODE_ENV
 });
--- rollbar/index.js
+++ rollbar/index.js
@@ -1,12 +1,19 @@
+var path    = require('path');
 var rollbar = require('rollbar');
 
 var rollbarConfig = {
 	codeVersion: process.env.npm_package_gitHead,
+	enabled:     process.env.NODE_ENV && process.env.ROLLBAR_ACCESS_TOKEN,
 	environment: process.env.ROLLBAR_ENV || process.env.NODE_ENV,
-	enabled:     process.env.NODE_ENV && process.env.ROLLBAR_ACCESS_TOKEN
+	verbose:     !process.env.NODE_ENV || !process.env.ROLLBAR_ACCESS_TOKEN,
+	root:        path.join(__dirname, '..')
 };
 rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN || ' ', rollbarConfig);
 
 rollbar.config = rollbarConfig;
+
+if (!process.env.NODE_ENV) {
+	rollbar.handleError = console.log;
+}
 
 module.exports = rollbar;
